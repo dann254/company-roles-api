f0d210e6566b071bf3dacd9fb95fe996781af751
Daniel Kipkemei <dann.professional@gmail.com>
2021-02-15 18:31:49 +0300

    added serializer

---
+++ company_roles/api/serializers.py
@@ -0,0 +1,59 @@
+
+from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'password'
+        )
+
+    def create(self, validated_data):
+        auth_user = User.objects.create_user(**validated_data)
+        return auth_user
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=128, write_only=True)
+    access = serializers.CharField(read_only=True)
+    refresh = serializers.CharField(read_only=True)
+    role = serializers.CharField(read_only=True)
+
+    def validate(self, data):
+        email = data['email']
+        password = data['password']
+        user = authenticate(email=email, password=password)
+
+        if user is None:
+            raise serializers.ValidationError("Invalid email or password")
+
+        try:
+            refresh = RefreshToken.for_user(user)
+            refresh_token = str(refresh)
+            access_token = str(refresh.access_token)
+
+            update_last_login(None, user)
+
+            validation = {
+                'access': access_token,
+                'refresh': refresh_token,
+                'email': user.email,
+                'role': user.role,
+            }
+
+            return validation
+        except AuthUser.DoesNotExist:
+            raise serializers.ValidationError("Invalid email or password")
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'role'
+        )
